3e0ed51616e6c6a81e1b176e8cb481319d2f9ba1
Jason Steenblik <steenblik@google.com>
2026-03-13 20:51:02 +0000

    Add test iam to the common folder

--- folder_common.tf
+++ folder_common.tf
@@ -1,16 +1,23 @@
 module "folder_common" {
   source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/folder?ref=v52.0.0"
 
-  parent = "organizations/${var.organization.id}"
-  name   = "Common"
-  #folder_create = false
+  parent        = "organizations/${var.organization.id}"
+  name          = "Common"
+  folder_create = false
+
+  iam_bindings_additive = {
+    am1-storage-admin = {
+      member = "user:akashr@marvell.com"
+      role   = "roles/storage.viewer"
+    }
+  }
 
   # Authoritative assignment for this role on this folder
-  iam = {
-    "roles/compute.networkAdmin" = ["group:network-admins@marvell.com"]
-  }
+  #iam = {
+  #  "roles/compute.networkAdmin" = ["group:network-admins@marvell.com"]
+  #}
 
-  factories_config = {
-    org_policies = "org_policies/folders/common"
-  }
+  #factories_config = {
+  # org_policies = "org_policies/folders/common"
+  # }
 }
