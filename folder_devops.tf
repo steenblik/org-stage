d1a3dd6a02844aef3d6d087d625d2c431f4ee72f
Jason Steenblik <steenblik@google.com>
2026-03-13 20:40:59 +0000

    Update folders

--- folder_devops.tf
+++ folder_devops.tf
@@ -1,16 +1,16 @@
-module "folder_common" {
+module "folder_devops" {
   source = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/folder?ref=v52.0.0"
 
   parent = "organizations/${var.organization.id}"
-  name   = "Common"
+  name   = "DevOps"
   #folder_create = false
 
   # Authoritative assignment for this role on this folder
-  iam = {
-    "roles/compute.networkAdmin" = ["group:network-admins@marvell.com"]
-  }
+  #iam = {
+  #  "roles/compute.networkAdmin" = ["group:network-admins@marvell.com"]
+  #}
 
   factories_config = {
-    org_policies = "org_policies/folders/common"
+    org_policies = "org_policies/folders/devops"
   }
 }
