25c1d23c6bd47cde7368f8e3693d557c57e52f20
Jason Steenblik <steenblik@google.com>
2026-03-17 22:27:47 +0000

    updates to subnet restriction

--- project-stage/project.tf
+++ project-stage/project.tf
@@ -10,16 +10,11 @@ module "service-project" {
     host_project = var.shared_vpc_host_project_id
   }
 
-  org_policies = {
+org_policies = {
     "compute.restrictSharedVpcSubnetworks" = {
       rules = [{
         allow = {
-          # These subnet names are is shared with the subnet name in the shared-vpc module
-          # if the name format is changed it must be changed there as well.
-          values = [
-            for key, config in var.subnet_config :
-            "projects/${var.shared_vpc_config.host_project_id}/regions/${config.region}/subnetworks/${var.project_config.project_id}-${key}"
-          ]
+          values = var.subnet_restriction
         }
       }]
     }
